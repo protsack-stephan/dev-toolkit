e3483122fcbebbdf11729b82f86d4774172a77c5
sashah <sasha.h@speedandfunction.com>
2022-02-06 15:12:55 -0500

    Cover log with tests

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,42 +1,56 @@
+// Package log is a structured logger.
+// The package can be extended with logger library API.
 package log
 
 import (
 	"github.com/sirupsen/logrus"
 )
 
-var logger = logrus.New()
+type Logger interface {
+	SetFormatter(formatter logrus.Formatter)
+	Printf(format string, args ...interface{})
+	Println(args ...interface{})
+	Panic(args ...interface{})
+	Error(args ...interface{})
+	Exit(code int)
+}
+
+var logger Logger
 
 func init() {
+	logger = logrus.New()
+
 	logger.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
-	logger.SetReportCaller(true)
 }
 
+// SetLogger sets the logger instance
+func SetLogger(l Logger) {
+	logger = l
+}
+
+// Printf call logrus Printf
 func Printf(format string, args ...interface{}) {
 	logger.Printf(format, args...)
 }
 
+// Println call logrus Println
 func Println(args ...interface{}) {
 	logger.Println(args...)
 }
 
+// Panic call logrus Panic
 func Panic(args ...interface{}) {
 	logger.Panic(args...)
 }
 
-func Debug(args ...interface{}) {
-	logger.Debug(args...)
-}
-
-func Warning(args ...interface{}) {
-	logger.Warning(args...)
-}
-
+// Error call logrus Error
 func Error(args ...interface{}) {
 	logger.Error(args...)
 }
 
+// Exit call logrus Exit
 func Exit(code int) {
-	logger.Error(code)
+	logger.Exit(code)
 }
--- pkg/log/log_test.go
+++ pkg/log/log_test.go
@@ -1,1 +1,67 @@
 package log
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+// Mock object for logger
+type loggerMock struct {
+	mock.Mock
+}
+
+// Mock logger SetFormatter method behaviour
+func (l *loggerMock) SetFormatter(formatter logrus.Formatter) {
+	l.Called(formatter)
+}
+
+// Mock logger Printf method behaviour
+func (l *loggerMock) Printf(format string, args ...interface{}) {
+	l.Called(format, args)
+}
+
+// Mock logger Println method behaviour
+func (l *loggerMock) Println(args ...interface{}) {
+	l.Called(args)
+}
+
+// Mock logger Panic method behaviour
+func (l *loggerMock) Panic(args ...interface{}) {
+	l.Called(args)
+}
+
+// Mock logger Error method behaviour
+func (l *loggerMock) Error(args ...interface{}) {
+	l.Called(args)
+}
+
+// Mock logger Exit method behaviour
+func (l *loggerMock) Exit(code int) {
+	l.Called(code)
+}
+
+const logTestPrintfFormat = "Format"
+const logTestPrintfLog = "run Printf"
+const logTestPrintlnLog = "run Println"
+const logTestPanicLog = "run Panic"
+const logTestErrorLog = "run Error"
+const logTestExitCode = 1
+
+func TestLogger(t *testing.T) {
+	loggerObj := new(loggerMock)
+
+	loggerObj.On("Printf", logTestPrintfFormat, []interface{}{logTestPrintfLog})
+	loggerObj.On("Println", []interface{}{logTestPrintlnLog})
+	loggerObj.On("Panic", []interface{}{logTestPanicLog})
+	loggerObj.On("Error", []interface{}{logTestErrorLog})
+	loggerObj.On("Exit", logTestExitCode)
+
+	SetLogger(loggerObj)
+
+	Printf(logTestPrintfFormat, logTestPrintfLog)
+	Println(logTestPrintlnLog)
+	Panic(logTestPanicLog)
+	Error(logTestErrorLog)
+	Exit(logTestExitCode)
+}
